130ee8fa19d5b67d41476af9b9de1182027974eb
cbeams <c@beams.io>
2024-05-18 22:46:17 +0200

    Write process id to pid file at startup

--- core/node/src/main/java/bisq/core/node/DataDir.java
+++ core/node/src/main/java/bisq/core/node/DataDir.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.channels.FileLock;
+import java.nio.file.Files;
 
 import static bisq.core.node.OptionsLog.log;
 import static java.lang.String.format;
@@ -14,6 +15,7 @@ class DataDir implements Closeable {
 
     private final File dir;
     private final FileLock lock;
+    private final File pidFile;
 
     public DataDir(Options options) {
         this.dir = options.dataDir();
@@ -40,17 +42,31 @@ class DataDir implements Closeable {
             throw new UncheckedIOException(ex);
         }
 
+        // Write current process id to pid file
+        long pid = ProcessHandle.current().pid();
+        this.pidFile = new File(dir,"bisq.pid");
+        try {
+            Files.writeString(pidFile.toPath() , String.valueOf(pid));
+        } catch (IOException ex) {
+            log.error("Error: unable to write process id ({}) to pid file '{}'", pidFile, pid, ex);
+        }
+
         // TODO: create log file
-        // TODO: create pid file
         // TODO: create args file
     }
 
     @Override
     public void close() {
+
+        log.debug("Releasing lock on data directory '{}'", dir);
         try {
             lock.release();
-        } catch (IOException ex) {
-            log.error("Error: unable to release lock on data directory '{}'", dir);
+        } catch (Exception ex) {
+            log.error("Error: unable to release lock on data directory '{}'", dir, ex);
         }
+
+        log.debug("Deleting pid file '{}'", pidFile);
+        if (!pidFile.delete())
+            log.error("Error: unable to delete pid file '{}'", pidFile);
     }
 }
